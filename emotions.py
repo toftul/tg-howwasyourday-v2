355a25ff9f94d9ed23b697f8026e739b04b2cda3
Ivan Toftul <toftul.ivan@gmail.com>
2023-09-27 16:48:21 +1000

    Russel based emotion spectrum

---
+++ emotions.py
@@ -0,0 +1,73 @@
+# Selected emotions from Fig. 4
+# Russell, J. A. A circumplex model of affect.
+# Journal of Personality and Social Psychology 39, 1161–1178 (1980)
+emotions_list = {
+    "Astonished": {
+        "valence": 0.158,
+        "arousal": 0.89
+    },
+    "Excited": {
+        "valence": 0.6176129779837776,
+        "arousal": 0.757094211123723
+    },
+    "Happy": {
+        "valence": 0.8261877172653533,
+        "arousal": 0.5255391600454029
+    },
+    "Pleased": {
+        "valence": 0.7705677867902669,
+        "arousal": 0.1850170261066969
+    },
+    "Relaxed": {
+        "valence": 0.689455388180765,
+        "arousal": -0.45289443813847896
+    },
+    "Peaceful": { # at ease
+        "valence": 0.5898030127462344,
+        "arousal": -0.2599318955732124
+    },
+    "Calm": {
+        "valence": 0.5619930475086907,
+        "arousal": -0.30533484676503986
+    },
+    "Sleepy": {
+        "valence": 0.27925840092699894,
+        "arousal": -0.6708286038592508
+    },
+    "Tired": {
+        "valence": 0.042873696407879525,
+        "arousal": -0.6276958002270148
+    },
+    "Bored": {
+        "valence": -0.6639629200463498,
+        "arousal": -0.6345062429057888
+    },
+    "Sad": {
+        "valence": -0.6477404403244494,
+        "arousal": -0.26674233825198645
+    },
+    "Miserable": {
+        "valence": -0.6964078794901506,
+        "arousal": -0.07832009080590252
+    },
+    "Nervous": {
+        "valence": -0.6315179606025492,
+        "arousal": 0.26901248581157766
+    },
+    "Angry": {
+        "valence": -0.5225955967555039,
+        "arousal": 0.5845629965947786
+    },
+    "Frustrated": {
+        "valence": -0.6454229432213209,
+        "arousal": 0.5096481271282632
+    },
+    "Annoyed": {
+        "valence": -0.545,
+        "arousal": 0.412
+    },
+    "Afraid": {
+        "valence": -0.34,
+        "arousal": 0.64
+    },
+}
