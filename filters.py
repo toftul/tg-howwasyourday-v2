6913ec40f934759ddd679793f5368f190b4f673c
Ivan <toftul.ivan@gmail.com>
2023-09-30 12:49:11 +1000

    filer for to check digits

--- filters.py
+++ filters.py
@@ -27,3 +27,20 @@ class FilterEmotions(MessageFilter):
         if not is_known_emotion:
             logging.error(f"Emotion {text} is not known (chat_id={chat_id})")
         return is_known_emotion
+
+
+class FilterIsDigit(MessageFilter):
+    def __init__(self):
+        super().__init__()
+
+    def filter(self, message: Message) -> bool:
+        text = message.text
+        chat_id = str(message.chat.id)
+        # check if it is a number
+        # https://stackoverflow.com/a/23639915/10282471
+        isdigit = text.replace('.','',1).replace('-','',1).replace('+','',1).isdigit()
+        if not isdigit:
+            logging.error(f"Got {text}, which is not a number (chat_id={chat_id}).")
+        
+        logging.error(f"Got {text}, which is a number (chat_id={chat_id}).")
+        return isdigit
