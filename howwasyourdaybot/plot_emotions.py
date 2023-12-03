04b8472ede1ce9ff4d53b49a1739eea0c6902775
Ivan Toftul <toftul.ivan@gmail.com>
2023-12-03 17:06:10 +1100

    multilang emotions plot

--- howwasyourdaybot/plot_emotions.py
+++ howwasyourdaybot/plot_emotions.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from emotions import emotions_list
-
+# get translations
+from phrases_multilang import emotions_translations, plot_words
  
-def plot_emotions():
+def plot_emotions(base_filename='emotions', lang='en'):
     emotions = list(emotions_list.keys())
     valence = [emotion_data["valence"] for emotion_data in emotions_list.values()]
     arousal = [emotion_data["arousal"] for emotion_data in emotions_list.values()]
@@ -12,24 +13,27 @@ def plot_emotions():
     plt.scatter(valence, arousal, marker='o', s=80, c=np.arctan2(valence, arousal), cmap='hsv', label='Emotions')
 
     for i, emotion in enumerate(emotions):
-        plt.annotate(emotion, (valence[i], arousal[i]), textcoords="offset points", xytext=(0, 10), ha='center')
+        #plt.annotate(emotion, (valence[i], arousal[i]), textcoords="offset points", xytext=(0, 10), ha='center')
+        plt.annotate(emotions_translations[emotion][lang], (valence[i], arousal[i]), textcoords="offset points", xytext=(0, 10), ha='center')
 
     #plt.gca().set_aspect('equal', adjustable='box')
 
-    plt.xlabel('Valence', weight='bold')
-    plt.ylabel('Arousal', weight='bold')
+    plt.xlabel(plot_words['valence'][lang], weight='bold')
+    plt.ylabel(plot_words['arousal'][lang], weight='bold')
 
-    plt.title('Emotions')
+    plt.title(plot_words['emotions'][lang])
     plt.xlim(-1.2, 1.2)
     plt.ylim(-1.2, 1.2)
     plt.tick_params(direction='in')
 
-    plt.xticks([-1, 0, 1], ['Negative', 'Neutral', 'Positive'])
-    plt.yticks([-1, 0, 1], ['Weak', 'Neutral', 'Strong'])
+    plt.xticks([-1, 0, 1], [plot_words['negative'][lang], plot_words['neutral'][lang], plot_words['positive'][lang]])
+    plt.yticks([-1, 0, 1], [plot_words['weak'][lang], plot_words['neutral'][lang], plot_words['strong'][lang]])
     #plt.grid(True, linestyle='--', alpha=0.6)
     plt.gca().set_aspect('equal', adjustable='box')
     plt.tight_layout()
-    plt.savefig('emotions.png')
+
+    filename = base_filename + '_' + lang + '.png'
+    plt.savefig(filename)
     #plt.show()
     
 
